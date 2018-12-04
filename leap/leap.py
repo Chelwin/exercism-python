b1e43e5dadcd33de3f24617457d904e13fc98b0c
vsanc <Chelwin@users.noreply.github.com>
2018-12-04 18:42:45 +0530

    exercism 1, 2

---
+++ leap/leap.py
@@ -0,0 +1,14 @@
+def is_leap_year(year):
+    #on every year that is evenly divisible by 4
+    #except every year that is evenly divisible by 100
+    #unless the year is also evenly divisible by 400
+    leap = False
+
+    try:
+        if (year%4)==0 and not((year%100)==0):
+            leap = True
+        elif (year%4)==0 and (year%400)==0:
+            leap = True
+    except:
+        raise Exception("Bad Argument: accept ints only") 
+    return leap
