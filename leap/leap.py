882b52308d7cff2c38bf3028823cad723b9bd938
vsanc <Chelwin@users.noreply.github.com>
2019-04-28 14:58:14 +0530

    improve leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,15 +1,54 @@
-def is_leap_year(year):
-    #on every year that is evenly divisible by 4
-    #except every year that is evenly divisible by 100
-    #unless the year is also evenly divisible by 400
-    leap = False
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Created on Sunday, 28 April 2019 at 14:36:57
 
-    try:
-        if (year%4)==0 and not((year%100)==0):
-            leap = True
-        elif (year%4)==0 and (year%400)==0:
-            leap = True
-    except:
-        raise Exception("Bad Argument: accept ints only") 
+@author: vsanc
+"""
+
+
+#def is_leap_year(year):
+#    if not isinstance(year, int):
+#        raise ValueError("Please enter an integer")
+#
+#    return year%4 == 0 and (year%100 != 0 or year%400 == 0)
+
+
+
+
+def is_leap_year(year):
+    leap = False
+    if year%400 == 0:
+        leap = True
+    elif year%100 == 0:
+        leap = False
+    elif year%4 == 0:
+        leap = True
     return leap
+
+
+
+data = []
+true = []
+file_name = "leap.in"
+
+try:
+   fh = open(file_name, 'r')
+except IOError:
+   print("Cannot open",file_name)
+else:
+   for new in fh:
+       if new != '\n':
+           addIt = new[:-1].split(',')   # remove trailing new line char
+#           print(addIt) 
+           data.extend(addIt)
+#   print(data)
+finally:
+   fh.close() 
+
+if data:
+    for year in data:
+        true.append(is_leap_year(int(year)))
+
+print(all(true))   # check if all leap years are detected correctly, prints True
 
