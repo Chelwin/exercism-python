4c4c8161b8bd2fce607aa7e4fc82eebef3ae3676
vsanc <Chelwin@users.noreply.github.com>
2019-04-28 16:45:26 +0530

    improve program logic

--- leap/leap.py
+++ leap/leap.py
@@ -7,27 +7,18 @@ Created on Sunday, 28 April 2019 at 14:36:57
 """
 
 
-#def is_leap_year(year):
-#    if not isinstance(year, int):
-#        raise ValueError("Please enter an integer")
-#
-#    return year%4 == 0 and (year%100 != 0 or year%400 == 0)
-
-
-
-
 def is_leap_year(year):
-    leap = False
-    if year%400 == 0:
-        leap = True
-    elif year%100 == 0:
-        leap = False
-    elif year%4 == 0:
-        leap = True
-    return leap
+    if not isinstance(year, int):
+        raise ValueError("Please enter an integer!")
+
+# A leap year must follow  at least (logical OR) one of the two paths:
+#    Be divisible by 400; i.e. by 4 AND 100 
+#    Be divisible by 4; i.e. by 4 AND NOT 100
+    return year%400 == 0 or (year%4 == 0 and year%100 != 0)
 
 
 
+########### TEST SUITE ###########
 data = []
 true = []
 file_name = "leap.in"
