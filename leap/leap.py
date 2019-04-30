5f134583a927b7d72c7f6c882d35c0cb44478892
vsanc <Chelwin@users.noreply.github.com>
2019-04-30 21:00:32 +0530

    improved procedures

--- leap/leap.py
+++ leap/leap.py
@@ -11,31 +11,7 @@ def is_leap_year(year):
     if not isinstance(year, int):
         raise ValueError("Please enter an integer!")
 
-# A leap year must follow  at least (logical OR) one of the two paths:
-#    Be divisible by 400; i.e. by 4 AND 100
-#    Be divisible by 4; i.e. by 4 AND NOT 100
+    # A leap year must follow  at least (logical OR) one of the two paths:
+    #    Be divisible by 400; i.e. by 4 AND 100
+    #    Be divisible by 4; i.e. by 4 AND NOT 100
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
-
-
-# TEST SUITE #
-data = []
-true = []
-file_name = input("Enter path containing the leap years: ")
-
-try:
-    fh = open(file_name, 'r')
-except IOError:
-    print("Cannot open", file_name)
-else:
-    for new in fh:
-        if new != '\n':
-            addIt = new[:-1].split(',')   # remove trailing new line char
-            data.extend(addIt)
-finally:
-    fh.close()
-
-if data:
-    for year in data:
-        true.append(is_leap_year(int(year)))
-
-print(all(true))   # if all leap years are detected correctly, prints True
--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,20 +1,19 @@
 #!usr/bin/env python3
-#-*- coding: utf-8 -*-
+#  -*- coding: utf-8 -*-
 
 """
 Created on Sunday, 28 April 2019 at 19:32:32
 
 @author: vsanc
 """
+
+
 def to_rna(dna_strand):
-    rna_list = []
-    for ch in dna_strand:
-        rna_list.extend('C' if ch=='G' else 'G' if ch=='C' else 'A' if ch=='T' else 'U',end=' ')
-    return rna_list
- 
 
+    nucleotides = set('GCTA')
+    if set(dna_strand).difference(nucleotides):
+        raise ValueError("The DNA strand has foreign nucleotide/s!")
 
+    replace_dict = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
 
-###### TEST SUITE #######
-dna_strand = 'GGTA'
-print(to_rna(dna_strand))
+    return ''.join(list(map(lambda x: replace_dict[x], dna_strand)))
