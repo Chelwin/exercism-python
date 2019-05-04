60d0113efcd467b680174c8c687fc3388ab77aac
vsanc <Chelwin@users.noreply.github.com>
2019-05-04 23:12:25 +0530

    iter 1 to grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,12 @@
 def on_square(integer_number):
-    pass
+    if (integer_number < 1 or integer_number > 64):
+        raise ValueError("Not a valid square")
+
+    return pow(2, (integer_number-1))
 
 
 def total_after(integer_number):
-    pass
+    if (integer_number < 1 or integer_number > 64):
+        raise ValueError("Not a valid square")
+
+    return sum([pow(2, x) for x in range(0, integer_number)])
