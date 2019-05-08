fb750d4180460fad863957849313dc0ab7db12a5
vsanc <Chelwin@users.noreply.github.com>
2019-05-08 10:35:51 +0530

    improved procedure

--- grains/grains.py
+++ grains/grains.py
@@ -9,4 +9,5 @@ def total_after(integer_number):
     if (integer_number < 1 or integer_number > 64):
         raise ValueError("Not a valid square")
 
-    return sum([pow(2, x) for x in range(0, integer_number)])
+    return sum([on_square(x) for x in range(1, integer_number + 1)])
+#    return sum([pow(2, x) for x in range(0, integer_number)])
