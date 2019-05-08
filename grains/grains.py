e09b6b872264f5882c6cf6f668be51aa4b00cdd6
vsanc <Chelwin@users.noreply.github.com>
2019-05-08 10:37:56 +0530

    cleaned code

--- grains/grains.py
+++ grains/grains.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday, 7 May 2019 at 20:42:50
+
+@author: vsanc
+"""
+
+
 def on_square(integer_number):
     if (integer_number < 1 or integer_number > 64):
         raise ValueError("Not a valid square")
@@ -10,4 +19,3 @@ def total_after(integer_number):
         raise ValueError("Not a valid square")
 
     return sum([on_square(x) for x in range(1, integer_number + 1)])
-#    return sum([pow(2, x) for x in range(0, integer_number)])
